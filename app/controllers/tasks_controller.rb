41f87797a0ce47ea4376d55227b39c91d6b2adbe
staff <staff@epicodus.com>
2016-09-07 10:32:53 -0700

    complete CRUD for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,10 +9,33 @@ class TasksController < ApplicationController
     @list = List.find(params[:list_id])
     @task = @list.tasks.new(task_params)
     if @task.save
-      redirect_to list_path(@task.list)
+      redirect_to list_path(@list)
     else
       render :new
     end
+  end
+
+  def edit
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @task= Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to list_path(@list)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@list)
   end
 
 private
