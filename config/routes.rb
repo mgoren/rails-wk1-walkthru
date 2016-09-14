4f33fa1ab9af35f60d3f47c338353b2436d1f7c9
Staff <epicodus_staff@staff-7.local>
2016-09-14 09:01:17 -0700

    Revert "add user auth manually with bcrypt"
    This reverts commit 8263966edafd0a8cf2c7a0776fe10bbc4756f65e.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'lists#index'
 
-  get "/sign_in" => "sessions#new"
-  post "/sign_in" => "sessions#create"
-  get "/sign_out" => "sessions#destroy"
-
-  resources :users, only: [:new, :create]
-
   resources :lists do
     resources :tasks, :except => [:show, :index]
   end
